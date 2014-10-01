4a210b96ae0c7687be743bf543620b67613b6021
child_ron <andrei_1410918@mail.ru>
2014-10-01 20:10:49 +0300

    remove useless files

--- src/app/modules/admin/controllers/userListCtrl.js
+++
@@ -1,8 +0,0 @@
-(function (angular, jcs) {
-    'use strict';
-
-    angular.module(jcs.modules.admin.name).controller(jcs.modules.admin.controllers.users, [
-        function () {
-        }
-    ]);
-}(angular, jcs));
--- src/common/pages/controllers/defaultCtrl.js
+++
@@ -1,9 +0,0 @@
-(function (angular, jcs) {
-    'use strict';
-
-    angular.module(jcs.modules.pages.name)
-        .controller(jcs.modules.pages.controllers.default, [
-        function () {
-        }
-    ]);
-}(angular, jcs));
