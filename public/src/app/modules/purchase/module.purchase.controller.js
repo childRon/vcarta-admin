ae6f10f39cdad8964b260a8bae432ad7452a789f
child_ron <andrei_1410918@mail.ru>
2014-10-13 22:42:36 +0300

    add purchase form

--- public/src/app/modules/purchase/module.purchase.controller.js
+++ public/src/app/modules/purchase/module.purchase.controller.js
@@ -1,33 +1,42 @@
 (function (angular, jcs) {
     'use strict';
 
-    angular.module(jcs.modules.purchase.name).controller(jcs.modules.purchase.controllers.purchase, 
-    	['$http','$scope', '$timeout', jcs.modules.purchase.factory.json, 
-    		function($http, $scope, $timeout, purchaseJsonFactory){
-			  
-			  $scope.dataObj = [];
-			  $scope.isLoading = false;
-			  $scope.rowCollection = [];
-			  $scope.dataObj = {};
-			  $scope.columns=['created_at', 'point','discount','bonus','final_sum'];
-			  var testRows = [];	
-			  
-			  purchaseJsonFactory.getPurchaseStuff().then(function (response) {
-		        var data = response.data;
-		        $scope.dataObj = data;
+    angular.module(jcs.modules.purchase.name).controller(jcs.modules.purchase.controllers.purchase,
+        ['$http', '$scope', '$timeout', jcs.modules.purchase.factory.json,
+            function ($http, $scope, $timeout, purchaseJsonFactory) {
+                var startDateObj = new Date();
+                $scope.startDate = startDateObj;
+                $scope.endDate = new Date(new Date().setMonth(startDateObj.getMonth() - 1));
+                $scope.dataObj = [];
+                $scope.isLoading = false;
+                $scope.rowCollection = [];
+                $scope.dataObj = {};
+                $scope.sum = 0;
+                $scope.count = 0;
+                $scope.columns = ['created_at', 'point', 'discount', 'bonus', 'final_sum'];
+                var testRows = [];
 
-		        $scope.isLoading = true;
-		        $scope.rowCollection = data.purchases;	
-		        $scope.displayedCollection = [].concat($scope.rowCollection);
-		        return data.purchases;
-		      }, function (error) {
-	                console.error(error);
-			  });
+                purchaseJsonFactory.getPurchaseStuff().then(function (response) {
+                    var data = response.data;
+                    $scope.sum = data.total.sum;
+                    $scope.count = data.total.count;
 
+                    $scope.isLoading = true;
+                    $scope.rowCollection = data.purchases;
+                    $scope.displayedCollection = [].concat($scope.rowCollection);
+                    return data.purchases;
+                }, function (error) {
+                    console.error(error);
+                });
 
-  		  }
-  		]
-  	);
+                $scope.submit = function () {
+                    alert(angular.toJson($scope.PurchaseForm));
+                }
+
+            }
+        ]
+    )
+    ;
 
 
 }(angular, jcs));
--- public/src/app/modules/purchase/module.purchase.factory.js
+++ public/src/app/modules/purchase/module.purchase.factory.js
@@ -1,11 +1,15 @@
 (function (angular, jcs) {
     'use strict';
 
-   angular.module(jcs.modules.purchase.name).factory(jcs.modules.purchase.factory.json, function ($q, $http) {
+   angular.module(jcs.modules.purchase.name).factory(jcs.modules.purchase.factory.json, function ($q, $http, $cookies) {
         return {
             getPurchaseStuff: function () {
+                var sid = $cookies.token;
+                console.log(sid) ;
+                var url = '/api2/purchases.json?sid:{0}'.format(sid);
+
                 var deferred = $q.defer(),
-                httpPromise = $http.get('json/purchase-filter.json');
+                httpPromise = $http.get(url);
 
                 httpPromise.then(function (response) {
                     deferred.resolve(response);
