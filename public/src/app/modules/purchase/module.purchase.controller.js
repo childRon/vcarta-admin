8aab16304837e36746190fc3ca4c8fb24f9228b4
childRon <andrei.misan@gmail.com>
2014-10-22 01:25:14 +0300

    implement filtering for purchase

--- public/src/app/modules/purchase/module.purchase.controller.js
+++ public/src/app/modules/purchase/module.purchase.controller.js
@@ -2,64 +2,65 @@
     'use strict';
 
     angular.module(jcs.modules.purchase.name).controller(jcs.modules.purchase.controllers.purchase,
-        ['$http', '$scope','eventbus', '$timeout', jcs.modules.purchase.factory.json,'scopeService',
-            function ($http, $scope, eventbus, $timeout, purchaseJsonFactory, scopeService) {
+        ['$http', '$scope','eventbus', '$timeout', jcs.modules.purchase.factory.json, '$filter',
+            function ($http, $scope, eventbus, $timeout, purchaseJsonFactory, $filter) {
+
                 var startDateObj = new Date();
-                $scope.startDate = startDateObj;
-                $scope.pattern = 'yyyy-MM-dd';
-                $scope.selectedDateAsNumber;
-                $scope.endDate = new Date(new Date().setMonth(startDateObj.getMonth() - 1));
+                var endDateObj = new Date(new Date().setMonth(startDateObj.getMonth() - 1));
+
+                var pattern = 'mediumDate';
+
+                var startDateValue = $filter('date')(startDateObj, pattern);
+                var endDateValue = $filter('date')(endDateObj, pattern);
+                var requestPattern = 'yyyy-MM-dd';
+
+                $scope.startDate = startDateValue;
+                $scope.endDate = endDateValue;
+
                 $scope.dataObj = [];
-                $scope.isLoading = false;
                 $scope.rowCollection = [];
                 $scope.dataObj = {};
                 $scope.sum = 0;
                 $scope.count = 0;
                 $scope.columns = ['created_at', 'point', 'discount', 'bonus', 'final_sum'];
-                var testRows = [];
-                $scope.$watch('selectedDateAsNumber', function() {
-                    console.log($scope.selectedDateAsNumber);
+                $scope.columnNames = ['Дата', 'Место', 'Сумма покупки', 'Сумма скидки', 'Итоговая сумма покупки'];
+
+
+
+
+                $scope.$watch('endDate', function() {
+                    endDateValue = $scope.endDate;
                 });
 
                 $scope.$watch('startDate', function() {
-                    console.log($scope.startDate);
-                });
-                purchaseJsonFactory.getPurchaseStuff().then(function (response) {
-                    var data = response.data;
-                    if(!data.total){
-                        return;
-                    }
-                    $scope.sum = data.total.sum;
-                    $scope.count = data.total.count;
-
-                    $scope.isLoading = true;
-                    $scope.rowCollection = data.purchases;
-                    $scope.displayedCollection = [].concat($scope.rowCollection);
-                    return data.purchases;
-                }, function (error) {
-                    console.error(error);
+                    startDateValue = $scope.startDate;
                 });
 
                 $scope.submit = function () {
-                    alert(angular.toJson($scope.startDate));
+
+                    var startDateValueReq = $filter('date')(new Date(startDateValue), requestPattern);
+                    var endDateValueReq = $filter('date')(new Date(endDateValue), requestPattern);
+
+                    purchaseJsonFactory.getPurchaseStuff(startDateValueReq, endDateValueReq).then(function (response) {
+                        var data = response.data;
+                        if(!data.total){
+                            return;
+                        }
+                        $scope.sum = data.total.sum;
+                        $scope.count = data.total.count;
+
+                        $scope.rowCollection = data.purchases;
+                        $scope.displayedCollection = [].concat($scope.rowCollection);
+                        return data.purchases;
+                    }, function (error) {
+                        console.error(error);
+                    });
+
                 }
             }
         ]
     )
-        .service('scopeService', function() {
-            return {
-                safeApply: function ($scope, fn) {
-                    var phase = $scope.$root.$$phase;
-                    if (phase == '$apply' || phase == '$digest') {
-                        if (fn && typeof fn === 'function') {
-                            fn();
-                        }
-                    } else {
-                        $scope.$apply(fn);
-                    }
-                }
-            };
-        })
+
     ;
 
 
--- public/src/app/modules/purchase/module.purchase.factory.js
+++ public/src/app/modules/purchase/module.purchase.factory.js
@@ -3,10 +3,15 @@
 
    angular.module(jcs.modules.purchase.name).factory(jcs.modules.purchase.factory.json, function ($q, $http, $cookies) {
         return {
-            getPurchaseStuff: function () {
+            getPurchaseStuff: function (date_from,date_to,offset,limit) {
                 var sid = $cookies.token;
                 console.log(sid) ;
-                var url = '/api2/purchases.json?sid:{0}'.format(sid);
+                var pagingParams = '';
+                if(offset && limit){
+                    pagingParams = ",offset:{3},limit:{4}".format(offset,limit);
+                }
+                var url = '/api2/purchases.json?sid:{0},date_from:{1},date_to:{2}{3}'.format(sid, date_from, date_to, pagingParams);
+
 
                 var deferred = $q.defer(),
                 httpPromise = $http.get(url);
